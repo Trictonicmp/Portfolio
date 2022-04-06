7d1954f8802f05d42ccca0e479324b544582e404
Trictonicmp <code_shadow@hotmail.com>
2022-04-06 12:54:11 -0500

    Add styles to workcards to support more than 4 ul li tags without breakign the neighbor elements

--- js/main.js
+++ js/main.js
@@ -30,7 +30,7 @@ const projectsDetails = [
     ScreenshotUrl: 'images/projects-screens/screen-1.png',
     ScreenshotAlt: 'Project image',
     Headline: 'Keepin track of hundreds of components x4',
-    Tags: ['codekit', 'github', 'javascript', 'x4'],
+    Tags: ['codekit', 'github', 'javascript', 'x4', 'github', 'javascript', 'x4', 'github'],
     Description: 'Unde facere minus sed perferendis, perspiciatis qui id provident quam dolore iusto, repellendus aperiam iure natus corruti suscipit facilis! Fugit, at vel. Quidem facilis ab necessitatibus rerum iusto ea? Facere, aut rerum officia hic quibusdam minus nam? Fuga sequi, minima debitis at velit id blanditiis porro cumque et delectus saepe aut magnam.',
     PreviewLink: 'http://preview4.com',
     SourceLink: 'http://sourcelink4.com',
